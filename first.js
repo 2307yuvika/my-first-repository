f8828ccb6a446fcd4547b6fc757c811107469b8e
2307yuvika <117252748+2307yuvika@users.noreply.github.com>
2022-11-08 18:33:14 +0530

    js

---
+++ first.js
@@ -0,0 +1,19 @@
+document.getElementById("demo").innerHTML =
+"The total is: " + total;
+document.getElementById("first").innerHTML = text;
+document.getElementById("para").innerHTML = num;
+function login()
+{
+var u=document.getElementById('username').value;
+
+if(u=='Abi')
+{
+document.getElementById('pa1').innerHTML='Valid';
+}
+else
+{
+document.getElementById('pa1').innerHTML='Invaild';
+}
+}
+document.getElementById("pa2").innerHTML =
+"The value of z is: " + z;
